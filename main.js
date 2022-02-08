80f8de4f306dd96f8491e635ded4323ab79bfb61
Favour Ifeanacho <favour.franki3@gmail.com>
2022-02-08 14:52:55 +0100

    first design prototype

---
+++ main.js
@@ -0,0 +1,9 @@
+const showMenuBtn = document.getElementById("show-menu-btn")
+const exitMenuBtn = document.getElementById("exit-menu-btn")
+const nav = document.getElementById("nav")
+showMenuBtn.addEventListener("click", ()=> {
+    nav.classList.toggle('nav')
+})
+exitMenuBtn.addEventListener("click", ()=> {
+    
+})
